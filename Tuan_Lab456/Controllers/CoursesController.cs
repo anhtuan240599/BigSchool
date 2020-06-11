6ce671e6d8cb57b4fe3db4259091818cd23abb40
Administrator <Administrator@PC245>
2020-06-11 14:15:42 +0700

    11/06/2020

--- Tuan_Lab456/Controllers/CoursesController.cs
+++ Tuan_Lab456/Controllers/CoursesController.cs
@@ -30,8 +30,14 @@ namespace Tuan_Lab456.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
             var course = new Course
             {
                 LecturerId = User.Identity.GetUserId(),
--- Tuan_Lab456/ViewModels/CourseViewModel.cs
+++ Tuan_Lab456/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Tuan_Lab456.Models;
@@ -8,8 +9,13 @@ namespace Tuan_Lab456.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
 
         public byte Category { get; set; }
